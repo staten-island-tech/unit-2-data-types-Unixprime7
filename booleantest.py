fd81196c284b4438d0c52417d184b18c2bcd53bc
Unixprime7 <michaeld801@nycstudents.net>
2026-02-03 09:26:39 -0500

    ok

---
+++ booleantest.py
@@ -0,0 +1,14 @@
+
+
+def discount(age, isMember, isResident):
+    if age < 12:
+        age = True
+    elif age >= 65:
+        age = True
+    elif isMember = 'yes'
+        isMember = True
+    elif isResident = 'yes'
+        isResident = True
+
+    if isResident or isMember or age:
+
--- oddeven.py
+++ oddeven.py
@@ -1,7 +1,7 @@
 number_value = input('insert a number')
-if number_value % 2 == 1:
+x = number_value % 2
+if x == 1:
     print('odd')
 else:
     print('even')
 
-'''fix the modulus'''
