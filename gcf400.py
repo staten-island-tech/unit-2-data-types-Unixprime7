d3ad06fbaf4f09f5dd21ee0c51acf2e416a666a9
Unixprime7 <michaeld801@nycstudents.net>
2026-02-09 09:27:10 -0500

    h

---
+++ gcf400.py
@@ -0,0 +1,12 @@
+number1 = input("State the first number: ")
+number_value1 = int(number1)
+
+number2 = input("State the second number: ")
+number_value2 = int(number2)
+
+def gcf(number_value1, number_value2):
+
+for i in range(number_value1):
+    x = number_value1 - i
+    if number_value1 % x == 0 and number_value2 % x == 0:
+gcf()
