35d420dae6ccc13319b34adef30c31cfee88ff88
Unixprime7 <michaeld801@nycstudents.net>
2026-02-10 08:58:42 -0500

    Update gcf400.py

--- gcf400.py
+++ gcf400.py
@@ -5,8 +5,10 @@ number2 = input("State the second number: ")
 number_value2 = int(number2)
 
 def gcf(number_value1, number_value2):
-
-for i in range(number_value1):
-    x = number_value1 - i
-    if number_value1 % x == 0 and number_value2 % x == 0:
-gcf()
+    for i in range(number_value1):
+        x = number_value1 - i
+        if number_value1 % x == 0 and number_value2 % x == 0:
+            y = int(x)
+            print(y)
+            return
+gcf(number_value1, number_value2)
